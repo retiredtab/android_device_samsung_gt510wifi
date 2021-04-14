706d2d1b25535ca7ed3306c434127c7dc15116fa
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-04-14 17:25:53 -0600

    Initial gt510 device upload

---
+++ device.mk
@@ -0,0 +1,33 @@
+#Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/gt510wifi/gt510wifi-vendor.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/device-common.mk)
+
+LOCAL_PATH := device/samsung/gt510wifi
+
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
+    $(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf
+
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/A05QF_sr544_module_info.xml \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/B05QF_sr544_module_info.xml \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/C05QF_sr544_module_info.xml
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/gt510wifi/overlay
---
+++ system_prop.mk
@@ -0,0 +1,31 @@
+# Google Play Store
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=196608
+
+# turn onscreen nav
+PRODUCT_PROPERTY_OVERRIDES += \
+	qemu.hw.mainkeys=0
+
+PRODUCT_PACKAGES += \
+	FlipFlap
+
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.cabl=0
+
+# Disable RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.carrier=wifi-only \
+	ro.radio.noril=1
+
+# RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=false
+
+# Screen
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+# Telephony
+PRODUCT_PROPERTY_OVERRIDES += \
+	telephony.lteOnCdmaDevice=0
