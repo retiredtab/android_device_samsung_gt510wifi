d79ba6503a564d1eb00ebf7b6694497444096192
Tim Zimmermann <tim@linux4.de>
2022-12-14 19:57:02 -0700

    gt5note10wifi: Uprev audio HAL to V7
    Updated audio policy by running:
    
    hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/samsung/universal8890-common/configs/audio/audio_policy_configuration.xml 6.0
    
    with some manual intervention.

--- device.mk
+++ device.mk
@@ -17,14 +17,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml
 
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio@6.0-service \
-    android.hardware.audio.effect@6.0-impl
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
    android.hardware.bluetooth.audio@2.0-impl \
