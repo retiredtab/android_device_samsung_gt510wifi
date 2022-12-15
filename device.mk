d2f3fc7bd51b63427206e136583e35ddc38e894b
ivanmeler <i_ivan@windowslive.com>
2022-12-14 20:02:40 -0700

    gt5note10wifi: add audio over bluetooth service

--- device.mk
+++ device.mk
@@ -26,7 +26,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
-   android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.audio.service.msm8916 \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default
 
 # Bluetooth
 PRODUCT_PACKAGES += \
